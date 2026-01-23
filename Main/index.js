5c01d296d108c79569ad8f6f848bd338991cc5e8
WilliamW09 <william.widenfalk@gmail.com>
2026-01-23 09:28:50 +0000

    [ADD] fixed darkmode

--- Main/index.js
+++ Main/index.js
@@ -2,6 +2,21 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
+    document.querySelector(".rubrik h1").classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) { localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+     }
 }
 
 document.querySelector("#darkBtn").addEventListener("click", toggleDarkMode);
+
+function loadTheme() {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark");
+    }
+}
+
+loadTheme();
