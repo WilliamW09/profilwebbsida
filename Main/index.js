57a6336201e0291aa23a692275054f3d01e97af1
WilliamW09 <william.widenfalk@gmail.com>
2026-01-27 11:49:27 +0100

    [ADD] hemmsida reset

--- Main/index.js
+++ Main/index.js
@@ -1,6 +1,9 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 
 function toggleDarkMode() {
+    localStorage.setItem("theme", "dark");
+    localStorage.setItem("lastVisit", date.now());
+
     document.body.classList.toggle("dark");
     document.querySelector(".rubrik h1").classList.toggle("dark");
 
@@ -16,6 +19,19 @@ function loadTheme() {
     const theme = localStorage.getItem("theme");
     if (theme === "dark") {
         document.body.classList.add("dark");
+    }
+}
+
+
+const lastVisit = localStorage.getItem("lastVisit");
+if (lastVisit) {
+    const FIVE_MINUTES = 1000 * 60 * 5;
+    const now = Date.now();
+    const timePassed = now - lastVisit;
+    if (timePassed > FIVE_MINUTES) {
+        localStorage.removeItem("theme");
+        localStorage.removeItem("lastVisit");  
+        
     }
 }
 
